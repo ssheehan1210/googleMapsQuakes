1ffe55c4418906aa28667d8f12a9b8468b2028b0
Jim Haff <Jimmyhaff3@gmail.com>
2017-09-28 07:36:46 -0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,4 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+const weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
-$(document).ready(function() {
-  console.log("Let's get coding!");
-  // CODE IN HERE!
 
-});
